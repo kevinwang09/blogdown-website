42efbb533569e4e3cac2a52d34cdb7eb92224862
Kevin Wang <kevinwang09@users.noreply.github.com>
2023-04-07 11:08:29 +1000

    update shell script

--- update_site.sh
+++ update_site.sh
@@ -9,12 +9,16 @@ then
   echo "Using blogdown to build the website"
   R -e "blogdown::build_site()"
   cd $source_code_repo/public
+  echo "Adding and committing all contents in the public repo"
+  git add -A
+  git commit -m "Update website"
   ls
   echo "Pushing public site to kevinwangstats"
   git push kws master
   echo "Pushing public site to kevinwang09"
   git push kw9 master
   echo "Pushing source code to kevinwangstats"
+  cd $source_code_repo
   git push kws master
   echo "Pushing source code to kevinwang09"
   git push kw9 master
